2a6b219d05c467ccae3452744c9c7cf8a81677cc
haya784 <hayatadane382@gmail.com>
2025-05-29 19:20:42 +0300

    Update attendance_intro_ui.py

--- attendance_intro_ui.py
+++ attendance_intro_ui.py
@@ -5,3 +5,12 @@ def welcome_message(): #define a function to welcome users(Lecturer)
     print("2. Exit")
     ch = input('Choose an option: ')
     return ch
+
+def teacher_name(): #define another function to get the teacher's name.
+     t_name = input("\nEnter your name: ")
+     
+     if t_name == '':
+      print('Teacher name must be filled.')
+      exit()
+      
+     return t_name # Return the teacher's name, making it usable in different files.
