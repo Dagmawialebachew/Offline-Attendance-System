69cfb0902908cd112e18ee266ce4f93952b5fb15
haya784 <hayatadane382@gmail.com>
2025-05-29 16:02:30 +0300

    Update attendance_into_ui.py

--- attendance_into_ui.py
+++ attendance_into_ui.py
@@ -1,1 +1,7 @@
-
+def welcome_message(): #define a function to welcome users(Lecturer)
+    
+    print(" Welcome to OAS--Offline Attendance System: ")
+    print("1. Take Attendance")
+    print("2. Exit")
+    ch = input('Choose an option: ')
+    return ch
