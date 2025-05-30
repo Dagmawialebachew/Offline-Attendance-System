d3dca68e3a2d2a7e039eec41b4b6939f7c58f35e
fkr-yosef <lovejossem@gmail.com>
2025-05-30 16:54:20 +0300

    Create attendance_saver.py

---
+++ attendance_saver.py
@@ -0,0 +1,20 @@
+import csv
+from attendance_intro_ui import teacher_name #importing the teacher name function from the attendance_intro_ui python file
+
+# This function saves the attendance records into a new CSV file
+def save_attendance(attendance, today):
+    # File name e.g. attendance_2025-05-29.csv
+    filename = teacher_name + "_attendance_" + today + ".csv"
+    
+    with open(filename, 'w', newline='') as file:  # Open file in write mode
+        writer = csv.writer(file)  # Use csv.writer to write to file
+        
+        # Write the header row (column names)
+        writer.writerow(['student_id', 'full_name', 'status'])
+        
+        # Write each attendance record to the file
+        for record in attendance:
+            writer.writerow(record)
+    
+    # Print confirmation message after saving
+    print(f"\n✅ Attendance saved in {filename}")
