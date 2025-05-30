9d2b355d9d4d34ae47597695441cdc5b511cb46c
Dagmawi Tewodros <dagmawialebachew@gmail.com>
2025-05-31 02:12:33 +0300

    Correct indentation problems

--- attendance_marker.py
+++ attendance_marker.py
@@ -1,7 +1,7 @@
 def mark_students(students):
   attendance_list = [] #Empty list to store each student's attendance record
   print("\nEnter P for Present, A for Absent\n")  # Instructions to the user
-    for student in students:
+  for student in students:
         student_id = student[0]
         full_name = student[1]
         
@@ -15,4 +15,4 @@ def mark_students(students):
         # Add the attendance record to the list (convert 'P' to 'Present', 'A' to 'Absent')
         attendance_list.append([student_id, full_name, 'Present' if status == 'P' else 'Absent'])
     
-    return attendance_list  # Return the full attendance list
+  return attendance_list  # Return the full attendance list
