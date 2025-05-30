ad3a6655fb0b4d79995bf872589079d9f28bcd43
kalkidanyou <birhanuk41@gmail.com>
2025-05-30 10:24:06 +0300

    Update attendance_read.py

--- attendance_read.py
+++ attendance_read.py
@@ -1,8 +1,13 @@
 import csv  #used to read from and write to CSV files (like Excel tables)
 
-# This functions reads student data from a CSV file
+# This function reads student data from a CSV file
 def read_students(filename):
-    students = []  # We make an empty list to store student informations
+    students = []  # We make an empty list to store student information
     with open(filename, 'r') as file:  # Open the file in read mode
         reader = csv.reader(file)      # Use csv.reader to read the file line by line..
         next(reader)  # This skips the first row (the header part --> student_id, full_name)
+        for row in reader:
+            student_id = row[0]      # Get the student ID from the first column(accessing values by index from a list.)
+            full_name = row[1]       # Get the full name from the second column -- list indexing.
+            students.append([student_id, full_name])  # Add to the list of students
+    return students  # Return the full list of students for further usage
